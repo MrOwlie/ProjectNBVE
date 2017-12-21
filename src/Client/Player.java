cb57fa066476212e15a8c2993f57f4c1ae3fee71
Anton Josefsson <Anton.1994-@hotmail.com>
2017-12-21 15:09:56 +0100

    Starting point for simulation/prediction

---
+++ src/Client/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import Client.MovingEntity;
+
+/**
+ *
+ * @author Anton
+ */
+public class Player extends MovingEntity{
+
+    @Override
+    public void correctPosition(float tpf) 
+    {
+        
+    }
+    
+}

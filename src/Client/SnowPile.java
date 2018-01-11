3d42d27b2c7865705b96a8d142f19175c92b7290
mrowlie <mrowlie@DESKTOP-BB53DH2>
2018-01-12 00:32:18 +0100

    Merge origin/master  + models Conflicts: 	src/server/Player.java

---
+++ src/Client/SnowPile.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author mrowlie
+ */
+public class SnowPile {
+    
+    
+    public SnowPile(AssetManager assetManager, Node node) {
+        
+    }
+}

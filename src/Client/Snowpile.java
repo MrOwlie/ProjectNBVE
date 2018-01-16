24ae287e4753ec2e5975d2312c5739acd6e69a21
mrowlie <mrowlie@DESKTOP-BB53DH2>
2018-01-16 20:43:11 +0100

    Merge origin/master
    Conflicts:
    	src/Client/MovingEntity.java
    	src/packets/Packet.java

---
+++ src/Client/Snowpile.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Dome;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.texture.Texture;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mrowlie
+ */
+public class Snowpile {
+    
+    public static ArrayList<Snowpile> snowpiles = new ArrayList();
+    
+    int id;
+    
+    Geometry geom;
+    Material snow;
+    
+    
+    
+    public Snowpile(AssetManager assetManager, Node node, int id, float x, float z) {
+        Sphere s = new Sphere(10, 10, 2);
+        
+        this.id = id;
+        
+        geom = new Geometry("Sphere", s);
+        snow = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        snow.setTexture("ColorMap", assetManager.loadTexture("Textures/snowmound.png"));
+        geom.setMaterial(snow);
+        geom.setLocalTranslation(x, 0, z);
+        node.attachChild(geom);
+    }
+}
